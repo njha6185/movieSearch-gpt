1d2052be8f9452b01123ff6f1c6b7ceda8fe8b53
NITISH KUMAR JHA <njha6185@gmail.com>
2024-09-16 20:10:25 +0530

    mobile version css for home page

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -6,8 +6,9 @@ function SecondaryContainer() {
   
   if(!movies) return
   return (
-    <div className=' bg-black'>
-      <div className='-mt-52 relative z-20'>
+    <div className='md:bg-black'>
+      <div className='from-current md:bg-transparent pt-[85%] md:pt-0 md:-mt-32 lg:-mt-52 relative md:z-20'>
+      <div className='bg-black md:bg-transparent'>
       <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovie}/>
       <MovieList title={"Popular Movie"} movies={movies?.popularmovie}/>
       <MovieList title={"Top rated"} movies={movies?.topRateMovie}/>
@@ -17,7 +18,7 @@ function SecondaryContainer() {
       <MovieList title={"On The Air series"} movies={movies?.onTheAirSeries}/>
       <MovieList title={"Popular Series"} movies={movies?.popularSeries}/>
       <MovieList title={"Top rated Series"} movies={movies?.topRatedSeries}/>
-      
+      </div>
     </div>
     </div>
   )
